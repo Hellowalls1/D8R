976341a37defebfea0fd0870bd763f51925980ad
Hellowalls1 <thesingingbutcher@gmail.com>
2020-09-07 21:13:27 -0500

    initial commit

---
+++ D8r/Profile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D8r
+{
+    class Profile
+    {
+        string name;
+        int age;
+        string city;
+        string country;
+        string pronouns;
+        string[] hobbies;
+
+        public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        //method to describe profile
+        public string ViewProfile()
+        {
+            string bio = $"Name = {name}\n Age = {age}\n City = {city}\n Country = {country}\n Pronouns = {pronouns}";
+
+            bio += "\nHobbies:\n";
+            foreach (string hobby in hobbies)
+            {
+                bio += $"- {hobby}\n";
+            }
+            return bio;
+        }
+        //public mehtod with a string array parameter
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
---
+++ D8r/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace D8r
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+
+            sam.SetHobbies(new string[] { "going to the store", "going for a walk", "going to the movies" });
+
+            Profile ellie = new Profile("Ellie Minkel", 27, "New York", "USA");
+            ellie.SetHobbies(new string[] { "zoom with friends", "cooking", "online shopping" });
+
+            Profile kelly = new Profile("Kelly Thompson", 28, "Long Island", "USA", "he/him");
+            kelly.SetHobbies(new string[] { "sk8ing", "watching old Kung-Fu movies", "pondering the meaning of life" });
+
+            Console.WriteLine("Welcome to D8R");
+            Console.Write("Are you 18? Answer y or n:  ");
+            string age = Console.ReadLine();
+
+            if (age == "n")
+            {
+                Console.WriteLine("Come back when you are 18!!!");
+            }
+            else
+
+            {
+                Console.WriteLine("Welcome to D8R");
+
+                void choiceMethod()
+                {
+
+
+                    Console.WriteLine("Active users are sam, ellie, and kelly.");
+                    Console.WriteLine("Enter one of their names to view their profile:");
+                    string selection = Console.ReadLine();
+                    if (selection == "sam")
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Here is Sam's profile.");
+                        Console.WriteLine(sam.ViewProfile());
+                        Console.WriteLine("Would you like to view another profile? Enter yes or no.");
+                        string answer = Console.ReadLine();
+
+                        if (answer == "yes")
+                        {
+                            choiceMethod();
+                        } else if (answer == "no")
+                        {
+                            Environment.Exit(0);
+                        } else
+                        {
+                            Console.WriteLine("You aren't taking your dating life seriously. Goodbye");
+                        }
+
+                    }
+                    else if (selection == "ellie")
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Here is Ellie's profile.");
+
+                        Console.WriteLine(ellie.ViewProfile());
+
+                        Console.WriteLine("Would you like to view another profile? Enter yes or no.");
+                        string answer = Console.ReadLine();
+
+                        if (answer == "yes")
+                        {
+                            choiceMethod();
+                        }
+                        else if (answer == "no")
+                        {
+                            Environment.Exit(0);
+                        }
+                        else
+                        {
+                            Console.WriteLine("You aren't taking your dating life seriously. Goodbye");
+                        }
+                    }
+                    else if (selection == "kelly")
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Here is Kelly's profile.");
+                        Console.WriteLine(kelly.ViewProfile());
+
+                        Console.WriteLine("Would you like to view another profile? Enter yes or no.");
+                        string answer = Console.ReadLine();
+
+                        if (answer == "yes")
+                        {
+                            choiceMethod();
+                        }
+                        else if (answer == "no")
+                        {
+                            Console.WriteLine("Thank you for using D8R! Goodbye!");
+                            Environment.Exit(0);
+                        }
+                        else
+
+                        {
+                            Console.WriteLine("You aren't taking your dating life seriously.Goodbye!!!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter an active user");
+                        choiceMethod();
+                    }
+                }
+                choiceMethod();
+            }
+
+
+
+
+
+
+        }
+    }
+}
